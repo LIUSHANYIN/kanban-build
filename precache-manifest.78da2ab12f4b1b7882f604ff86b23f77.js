43ad3e7ee3dda467aabd44e25323f2684fa22bcf
shanyin liu <liu.shany@husky.neu.edu>
2020-04-24 18:35:49 -0400

    kanbanboard

---
+++ precache-manifest.78da2ab12f4b1b7882f604ff86b23f77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbb2c8368c26017ad218c5db671b2db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "452b1831c7d34a30e990",
+    "url": "/static/css/main.7292ffa8.chunk.css"
+  },
+  {
+    "revision": "ec1890e321a5bb5cdaed",
+    "url": "/static/js/2.d1da8a04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d1da8a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452b1831c7d34a30e990",
+    "url": "/static/js/main.31957430.chunk.js"
+  },
+  {
+    "revision": "dbbedc5ed6d26c0144d3",
+    "url": "/static/js/runtime-main.cd219afe.js"
+  }
+]);
